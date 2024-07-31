881b3d52bb6d991298caaccb3ef64ce9f6b06c52
KomarabathiniNishitha <nishithanishaa1509@gmail.com>
2024-07-31 21:46:53 +0530

    Create Multipleblocks.java

---
+++ Multipleblocks.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Multipleblocks
+{
+    public static void main(String args[])
+    {
+        Scanner scn = new Scanner(System.in);
+        try
+        {
+            int n = Integer.parseInt(scn.nextLine());
+           
+            if (99%n == 0)
+                System.out.println(n + " is a factor of 99");
+        }
+        catch (ArithmeticException ex)
+        {
+            System.out.println("Arithmetic " + ex);
+        }
+        catch (NumberFormatException ex)
+        {
+            System.out.println("Number Format Exception " + ex);
+        }
+    
+finally
+{
+System.out.println("this is a finally block");
+}
+}
